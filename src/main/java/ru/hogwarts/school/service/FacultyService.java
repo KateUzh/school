0a5d6b59efce919b1c76cc3b32e4d6bf1b89cc90
HomeWorld <KateLusis@gmail.com>
2025-08-31 17:27:36 +0300

    Add hw1/

---
+++ src/main/java/ru/hogwarts/school/service/FacultyService.java
@@ -0,0 +1,51 @@
+package ru.hogwarts.school.service;
+
+import org.springframework.stereotype.Service;
+import ru.hogwarts.school.model.Faculty;
+import ru.hogwarts.school.model.Student;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class FacultyService {
+    Map<Long, Faculty> facultyMap = new HashMap<>();
+    long count = 0;
+
+    public Faculty addFaculty(Faculty faculty) {
+        faculty.setId(count++);
+        facultyMap.put(faculty.getId(), faculty);
+        return faculty;
+    }
+
+    public Faculty editFaculty(Faculty faculty) {
+        if (facultyMap.containsKey(faculty.getId())) {
+            facultyMap.put(faculty.getId(), faculty);
+        }
+        return faculty;
+    }
+
+    public Map<Long, Faculty> getFaculty() {
+        return facultyMap;
+    }
+
+    public Faculty findFaculty(Long id) {
+        return facultyMap.get(id);
+    }
+
+    public Faculty deleteFaculty(Long id) {
+        return facultyMap.remove(id);
+    }
+
+    public Collection<Faculty> findFacultyByColor(String color) {
+        ArrayList<Faculty> result = new ArrayList<>();
+        for (Faculty faculty : facultyMap.values()) {
+            if (faculty.getColor().equalsIgnoreCase(color)) {
+                result.add(faculty);
+            }
+        }
+        return result;
+    }
+}
---
+++ src/main/java/ru/hogwarts/school/service/StudentService.java
@@ -0,0 +1,51 @@
+package ru.hogwarts.school.service;
+
+import org.springframework.stereotype.Service;
+import ru.hogwarts.school.model.Student;
+
+import java.util.*;
+
+@Service
+public class StudentService {
+    private final Map<Long, Student> studentMap = new HashMap<>();
+    private long count = 0;
+
+    public Student addStudent(Student student) {
+        student.setId(count++);
+        studentMap.put(student.getId(), student);
+        return student;
+    }
+
+    public Student editStudent(Student student) {
+        if (studentMap.containsKey(student.getId())) {
+            studentMap.put(student.getId(), student);
+        }
+        return student;
+    }
+
+    public Map<Long, Student> getStudent() {
+        return studentMap;
+    }
+
+    public Student findStudent(long id) {
+        return studentMap.get(id);
+    }
+
+    public Student deleteStudent(long id) {
+        return studentMap.remove(id);
+    }
+
+    public Collection<Student> findStudentByAge(int age) {
+        ArrayList<Student> result = new ArrayList<>();
+        for (Student student : studentMap.values()) {
+            if (student.getAge() == age) {
+                result.add(student);
+            }
+        }
+        return result;
+    }
+
+    public Map<Long, Student> getAll() {
+        return new HashMap<>(studentMap);
+    }
+}
